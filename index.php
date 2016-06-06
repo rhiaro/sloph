47b41d42ee0d94d647055b05dfec764a3b796c2a
rhiaro <amy@rhiaro.co.uk>
2016-06-05 18:08:19 -0700

    Get from files

--- index.php
+++ index.php
@@ -2,11 +2,25 @@
 session_start();
 require_once('vendor/init.php');
 
-$q = query_select_s_desc(100);
-$r = execute_query($ep, $q);
-if($r){
-  $posts = construct_uris($ep, select_to_list($r, array("uri")));
+$files = scandir("data");
+$posts = array();
+foreach($files as $file){
+  try{
+    $ttl = file_get_contents("data/".$file);
+    $graph = new EasyRdf_Graph();
+    $graph->parse($ttl, 'ttl');
+    //var_dump($graph);
+    $posts[] = $graph->serialise('php');
+  }catch(Exception $e){
+    echo $file;
+  }
 }
+
+// $q = query_select_s_desc(100);
+// $r = execute_query($ep, $q);
+// if($r){
+//   $posts = construct_uris($ep, select_to_list($r, array("uri")));
+// }
 
 ?>
 <!doctype html>
@@ -18,11 +32,11 @@ if($r){
     <h1>Sloph</h1>
     <?foreach($posts as $uri => $post):?>
       <article>
-        <h2><a href="resource.php?resource=<?=$uri?>"><?=$uri?></a></h2>
+        <h2><a href="<?=$uri?>"><?=$uri?></a></h2>
         <?foreach($post as $k => $vs):?>
           <p><strong><?=$k?>: </strong>
            <?foreach($vs as $v):?>
-              <?=$v['value']?>, 
+              <?=var_dump($v)?>, 
            <?endforeach?>
           </p>
         <?endforeach?>
