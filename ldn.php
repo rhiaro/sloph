206825fc9978bcf769f8846056541737e59f4dd3
rhiaro <amy@rhiaro.co.uk>
2016-07-06 12:49:59 -0400

    Check content-type headers for POST and GET. Add @id to payload for new resource created

--- ldn.php
+++ ldn.php
@@ -1,49 +1,68 @@
 <?
 
 function write($data, $date=null, $slug=null){
-  $json = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-  
+  $base = $_SERVER["HTTP_HOST"];
   if(!$slug) $slug = uniqid();
   if(!$date) $date = date("ymd-His");
     
   $log = "inbox/".$date."_".$slug.".json";
+  $data["@id"] = $base."/".$log;
+  $json = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
   $h = fopen($log, 'w');
-  fwrite($h, $data);
+  fwrite($h, $json);
   fclose($h);
   return $log;
 }
 
+function supported_content_type($ct){
+  $supported = false;
+  $cts = array("application/ld+json", "application/activity+json");
+  if(in_array($ct, $cts)){
+    $supported = true;
+  }
+  return $supported;
+}
+
+function valid_data($data){
+  $valid = false;
+  $parsed = json_decode($data, true);
+  if($parsed !== null){
+    $valid = true;
+    // TODO: Valid JSON-LD
+  }
+  return $valid;
+}
+
 /* Receiving */
 $msg = file_get_contents('php://input');
+$headers = apache_request_headers();
 if(isset($msg) && !empty($msg)){
-  $post = json_decode($msg, true);
-  if($post === null || !isset($post["@context"])){
-    // Not valid JSON or doesn't have a context so drop it
-    // TODO: check content-type header
+  if(!supported_content_type($headers["Content-Type"]) || !valid_data($msg)){
+    // Not valid JSON so drop it
     header("HTTP/1.1 415 Unsupported Media Type");
   }else{
-    $path = write($msg, date("ymd-His"));
+    $path = write(json_decode($msg, true), date("ymd-His"));
     header("HTTP/1.1 201 Created");
-    header("Location: $path");
+    header("Location: https://rhiaro.co.uk/$path");
   }
 }else{
 
   /* Serving */
-  // TODO: Check accept header
-  // If none or json
-  //   serve list as json
-  $files = scandir("inbox");
-  $notifs = array();
-  foreach($files as $file){
-    if(!is_dir($file) && $file != "." && $file != ".."){
-      $notifs[] = array("@id" => "http://rhiaro.co.uk/inbox/".$file);
+  if(supported_content_type($headers["Accept"])){
+    $files = scandir("inbox");
+    $notifs = array();
+    foreach($files as $file){
+      if(!is_dir($file) && $file != "." && $file != ".."){
+        $notifs[] = array("@id" => "http://rhiaro.co.uk/inbox/".$file);
+      }
     }
+    $inbox = array("@context" => "http://www.w3.org/ns/ldp", "@type" => "ldp:Inbox", "ldp:contains" => $notifs);
+    $inboxjson = json_encode($inbox, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    header("Content-Type: application/ld+json");
+    echo $inboxjson;
+  }else{
+    header("HTTP/1.1 415 Unsupported Media Type");
   }
-  $inbox = array("@context" => "http://www.w3.org/ns/ldp", "@type" => "ldp:Inbox", "ldp:contains" => $notifs);
-  $inboxjson = json_encode($inbox, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-  header("Content-Type: application/ld+json");
-  echo $inboxjson;
-  // else
-  //   return 415
 }
 ?>
