a5d4a3d7f21e5882af41e3d1b39c11ad37c36463
rhiaro <amy@rhiaro.co.uk>
2016-07-24 22:05:02 -0400

    Remove comment

--- resource.php
+++ resource.php
@@ -15,13 +15,6 @@ if(isset($_GET['resource'])){
   $result = get($ep, $resource, $ct);
   header($result['header']);
   echo $result['content'];
-  
-    // if($ct == "activity" || $ct == "activity+json" || $ct == "as2" || $ct == "as"){
-    //   header('Content-Type: application/activity+json');
-    //   echo graph_to_as2($graph);
-    //   exit;
-    // }
-  
 
 }else{
   $message["Beyond the final frontier"] = "You shouldn't be here.";
