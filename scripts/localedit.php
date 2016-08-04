a5266d3e953663a58a4280b16ac8a742ba976f67
rhiaro <amy@rhiaro.co.uk>
2016-08-04 17:37:02 -0400

    Fix encoding . in input names

--- scripts/localedit.php
+++ scripts/localedit.php
@@ -26,7 +26,7 @@ function plustype($i=0){
       "http://vocab.amy.so/blog#" => array("Consumption", "Acquisition")
     );
   $out = '<p><label><strong>+ type</strong>: </label>';
-  $out .= '  <select name="http://www.w3.org/1999/02/22-rdf-syntax-ns#type['.$i.'][value]">';
+  $out .= '  <select name="data[http://www.w3.org/1999/02/22-rdf-syntax-ns#type]['.$i.'][value]">';
   $out .= '    <option value="">none</option>';
   foreach($types as $base => $frags){
     foreach($frags as $type){
@@ -34,17 +34,33 @@ function plustype($i=0){
     }
   }
   $out .= '  </select>';
-  $out .= '<input type="hidden" name="http://www.w3.org/1999/02/22-rdf-syntax-ns#type['.$i.'][type]" value="uri" /></p>';
+  $out .= '<input type="hidden" name="data[http://www.w3.org/1999/02/22-rdf-syntax-ns#type]['.$i.'][type]" value="uri" /></p>';
   return $out;
 }
 
-if(count($_POST) > 0){
+function delete($ep, $uri){
+  $q = query_delete($uri);
+  //$r = execute_query($ep, $q);
+  return $q;
+}
+
+function insert($ep, $turtle){
+  $q = query_insert($turtle);
+  //$r = execute_query($ep, $q);
+  return $q;
+}
+
+if(count($_POST['data']) > 0){
+  $_POST = $_POST['data'];
   $newgraph = new EasyRdf_Graph($_POST['uri']);
   $uri = $_POST['uri'];
   unset($_POST['uri']);
   $rdfphp[$uri] = remove_empty($_POST);
   $newgraph->parse($rdfphp, 'php');
-  $result[$uri] = $newgraph->serialise('turtle');
+  var_dump($_POST);
+  $turtle = $newgraph->serialise('ntriples');
+  var_dump($turtle);
+  $result[$uri] = $turtle . "\n-------\n" . delete($ep, $uri) . "\n-------\n" . insert($ep, $turtle);
 }
 
 if(!isset($_SESSION['uris'])){
@@ -91,7 +107,7 @@ $posts = construct_uris($ep, $uris);
     <?foreach($posts as $uri => $post):?>
       <form id="<?=$uri?>" method="post" action="#<?=$uri?>">
         <p><a href="<?=$uri?>"><?=$uri?></a> <input type="submit" value="Save"/></p>
-        <input type="hidden" value="<?=$uri?>" name="uri" />
+        <input type="hidden" value="<?=$uri?>" name="data[uri]" />
         <?if(isset($result[$uri])):?>
           <div style="overflow:hidden; width: 100%;">
             <pre>
@@ -106,13 +122,13 @@ $posts = construct_uris($ep, $uris);
           <p><label><?=$k?>: </label>
            <?foreach($vs as $i => $v):?>
               <?if(strlen($v['value']) > 120):?>
-                <textarea name="<?=$k?>[<?=$i?>][value]"><?=$v['value']?></textarea>
+                <textarea name="data[<?=$k?>][<?=$i?>][value]"><?=$v['value']?></textarea>
               <?else:?>
-                <input name="<?=$k?>[<?=$i?>][value]" type="text" value="<?=$v['value']?>" style="width: <?=strlen($v['value']) * 8?>px; max-width: 100%" />
+                <input name="data[<?=$k?>][<?=$i?>][value]" type="text" value="<?=$v['value']?>" style="width: <?=strlen($v['value']) * 8?>px; max-width: 100%" />
               <?endif?>
-              <input name="<?=$k?>[<?=$i?>][type]" type="hidden" value="<?=$v["type"]?>" />
+              <input name="data[<?=$k?>][<?=$i?>][type]" type="hidden" value="<?=$v["type"]?>" />
               <?if(isset($v["datatype"])):?>
-                <input name="<?=$k?>[<?=$i?>][datatype]" type="text" value="<?=$v["datatype"]?>" />
+                <input name="data[<?=$k?>][<?=$i?>][datatype]" type="text" value="<?=$v["datatype"]?>" />
               <?endif?>
           <?endforeach?>
           </p>
@@ -121,12 +137,12 @@ $posts = construct_uris($ep, $uris);
               <?=plustype($i+1);?>
             <?else:?>
               <label><strong>+</strong></label>
-              <input type="text" name="<?=$k?>[<?=count($vs)?>][value]" style="width: 8em;" />
-              <select name="<?=$k?>[<?=count($vs)?>][type]">
+              <input type="text" name="data[<?=$k?>][<?=count($vs)?>][value]" style="width: 8em;" />
+              <select name="data[<?=$k?>][<?=count($vs)?>][type]">
                 <option value="literal">lit</option>
                 <option value="uri">uri</option>
               </select>
-              <select name="<?=$k?>[<?=count($vs)?>][datatype]">
+              <select name="data[<?=$k?>][<?=count($vs)?>][datatype]">
                 <option value="">none</option>
                 <option value="http://www.w3.org/2001/XMLSchema#dateTime">dateTime</option>
               </select>
@@ -136,5 +152,8 @@ $posts = construct_uris($ep, $uris);
       </form>
       <hr/>
     <?endforeach?>
+    <div class="info">
+      <p>Resources <?=$offset?> to <?=$offset+$length?> of <?=count($_SESSION['uris'])?> | <a href="?offset=<?=$offset-$length?>">prev</a> | <a href="?offset=<?=$offset+$length?>">next</a> | <a href="?reset=uris">reset</a></p>
+    </div>
   </body>
 </html>
