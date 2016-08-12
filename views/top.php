826645b89b6c1256501e99fe948ab1ae35e01f68
rhiaro <amy@rhiaro.co.uk>
2016-08-11 23:53:00 -0400

    Stylesheets and templates

---
+++ views/top.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+  <head>
+    <title><?=$resource->get('as:name')?></title>
+    <link rel="stylesheet" type="text/css" href="normalise.min.css" />
+    <link rel="stylesheet" type="text/css" href="<?=$resource->get('view:css')?>" />
+  </head>
+  <body>
