7e9d6ca301d2c702d80c35bc163f1f85160b6bb3
rhiaro <amy@rhiaro.co.uk>
2016-08-27 01:03:56 -0400

    Fix relative stylesheet URLs

--- views/top.php
+++ views/top.php
@@ -2,9 +2,9 @@
 <html>
   <head>
     <title><?=$resource->get('as:name')?></title>
-    <link rel="stylesheet" type="text/css" href="normalise.min.css" />
-    <link rel="stylesheet" type="text/css" href="views/core.css" />
-    <link rel="stylesheet" type="text/css" href="<?=$resource->get('view:stylesheet')?>" />
+    <link rel="stylesheet" type="text/css" href="/views/normalize.min.css" />
+    <link rel="stylesheet" type="text/css" href="/views/core.css" />
+    <link rel="stylesheet" type="text/css" href="/<?=$resource->get('view:stylesheet')?>" />
     <style>
       <?=$resource->get('view:css')?>
     </style>
