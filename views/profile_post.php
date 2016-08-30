57134d9bbe8eb282010ab634b6152056acaf11b4
rhiaro <amy@rhiaro.co.uk>
2016-08-30 16:16:52 -0400

    Make profile post dates nicer

--- views/profile_post.php
+++ views/profile_post.php
@@ -1,13 +1,13 @@
 <p><span><?=get_icon($resource)?></span> 
   <?if($resource->isA('asext:Consume')):?>
-  The last thing I ate was <?=$resource->get('as:name')?>, <a href="<?=$resource->getUri()?>"><?=time_ago($resource->get('as:published'))?></a>
+  The last thing I ate was <?=$resource->get('as:name')?>, 
   <?elseif($resource->isA('asext:Acquire')):?>
-  The last thing I acquired was <?=$resource->get('as:summary')?>, <a href="<?=$resource->getUri()?>"><?=time_ago($resource->get('as:published'))?></a>
+  The last thing I acquired was <?=$resource->get('as:summary')?>, 
   <?elseif($resource->isA('as:Arrive')):?>
     <?if($locations->get($resource->get('as:location'), 'blog:presentLabel')):?>
-    I am <a href="<?=$resource->get('as:location')?>"><?=$locations->get($resource->get('as:location'), 'blog:presentLabel')?></a> since <a href="<?=$resource->getUri()?>"><?=time_ago($resource->get('as:published'))?></a>
+    I am <a href="<?=$resource->get('as:location')?>"><?=$locations->get($resource->get('as:location'), 'blog:presentLabel')?></a> since 
     <?else:?>
-    I checked into <a href="<?=$resource->get('as:location')?>"><?=$resource->get('as:location')?></a>, <a href="<?=$resource->getUri()?>"><?=time_ago($resource->get('as:published'))?></a>
+    I checked into <a href="<?=$resource->get('as:location')?>"><?=$resource->get('as:location')?></a>, 
     <?endif?>
   <?elseif($resource->isA('as:Like')):?>
   The last thing I liked was <a href="<?=$resource->getUri()?>"><?=$resource->get('as:name') ? $resource->get('as:name') : $resource->get('as:object')?></a>
@@ -16,11 +16,11 @@
   <?elseif($resource->isA('as:Announce')):?>
   The last thing I reposted was <a href="<?=$resource->getUri()?>"><?=$resource->get('as:name') ? $resource->get('as:name') : $resource->get('as:object')?></a>
   <?elseif($resource->isA('as:Note')):?>
-    The last thing I scribbled, <a href="<?=$resource->getUri()?>"><?=time_ago($resource->get('as:published'))?></a>, was about <?=$resource->get('as:tag')?>
+    The last thing I scribbled was about <em><?=$resource->get('as:tag')?></em>,
   <?elseif($resource->isA('as:Article')):?>
-  The last article I wrote was <strong><?=$resource->get('as:name')?></strong> <time><a href="<?=$resource->getUri()?>"><?=time_ago($resource->get('as:published'))?></a></time>
+  The last article I wrote was <strong><?=$resource->get('as:name')?></strong>
   <?elseif($resource->isA('as:Travel')):?>
-  The last trip I planned was from <?=get_name($ep, $resource->get('as:origin'))?> on <?=$resource->get('as:startTime')?> to <?=get_name($ep, $resource->get('as:target'))?> at <?=$resource->get('as:endTime')?> <a href="<?=$resource->getUri()?>"><?=time_ago($resource->get('as:published'))?></a>
+  The last trip I planned was from <?=get_name($ep, $resource->get('as:origin'))?> on <?=$resource->get('as:startTime')?> to <?=get_name($ep, $resource->get('as:target'))?> at <?=$resource->get('as:endTime')?> 
   <?elseif($resource->isA('as:Accept')):?>
     <?
       $event = get($ep, $resource->get('as:object'));
@@ -34,6 +34,6 @@
       $start = new DateTime($event->get('as:startTime'));
       $end = new DateTime($event->get('as:endTime'));
     ?>
-    The last event I RSVP'd to was <strong><?=$event->get('as:name') ? $event->get('as:name') : $event->getUri() ?></strong>, taking place from <?=$start->format("d F")?> to <?=$end->format("d F")?> at <?=get_name($ep, $event->get('as:location'))?>,  <a href="<?=$resource->getUri()?>"><?=time_ago($resource->get('as:published'))?></a>
+    The last event I RSVP'd to was <strong><?=$event->get('as:name') ? $event->get('as:name') : $event->getUri() ?></strong>, taking place from <?=$start->format("d F")?> to <?=$end->format("d F")?> at <?=get_name($ep, $event->get('as:location'))?>,  
   <?endif?>
-</p>
+ <time><a href="<?=$resource->getUri()?>"><?=time_ago($resource->get('as:published'))?></a></time></p>
