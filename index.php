7f0508e59938f30bed90a8d0803eb44584d843dd
rhiaro <amy@rhiaro.co.uk>
2016-09-09 16:06:44 -0400

    Display latest posts

--- index.php
+++ index.php
@@ -15,11 +15,10 @@ try {
     echo $content;
   }else{
     $resource = $content->resource($relUri);
-    // $items = $content->all($relUri, 'as:items');
 
     $last_of_derp = array();
+    $latest_post_uris = array();
     $latest_posts = array();
-    // $all = array();
 
     $items = $content->toRdfPhp();
     $items = array_reverse($items);
@@ -45,7 +44,7 @@ try {
         }
 
         if($type == $ns->expand("as:Article") || $type == $ns->expand("as:Note")){
-          $latest_posts[] = $uri;
+          $latest_post_uris[] = $uri;
         }
 
         if($type == $ns->expand("as:Arrive")){
@@ -56,39 +55,6 @@ try {
 
       $items[$uri]['color'] = $color;
 
-    //   $uri = $item->getUri();
-    //   $types = $item->types();
-    //   if(empty($types)){
-    //     $types = array("as:Object");
-    //     $item->addResource("rdf:type", "as:Object");
-    //   }
-    //   foreach($types as $type){
-    //     if(!isset($last_of_derp[$type]) && $type != "as:Activity"){
-    //       $result = get($ep, $uri);
-    //       $content = $result['content'];
-    //       $resource = $content->resource($uri);
-    //       $last_of_derp[$type] = $resource;
-    //     }
-
-    //     if(($type == "as:Article" || $type == "as:Note") && count($latest_posts) <= 9){
-    //       $result = get($ep, $uri);
-    //       $content = $result['content'];
-    //       $resource = $content->resource($uri);
-    //       $latest_posts[] = $resource;
-    //     }
-    //   }
-
-    //   if($item->isA('as:Arrive')){
-    //     $res = get($ep, $uri);
-    //     $arrive = $res['content']->resource($uri);
-    //     $item->addResource("as:location", $arrive->get("as:location"));
-    //     if(!isset($currentlocation)){
-    //       $currentlocation = $item->get("as:location");
-    //     }
-    //   }
-
-    //   $all[] = $item;
-
     }
     if($locations){
       $wherestyle = "body, #me a:hover { background-color: ".$locations->get($currentlocation, 'view:color')."}\n";
@@ -97,11 +63,25 @@ try {
       }
     }
 
-    $items = array_reverse($items);
-    $latest_posts = array_reverse(array_slice($latest_posts, count($latest_posts)-9, 9));
-
     include 'views/top.php';
     include 'views/header.php';
+
+    $items = array_reverse($items);
+    $latest_post_uris = array_reverse(array_slice($latest_post_uris, count($latest_post_uris)-6, 6));
+    $next = array_pop($latest_post_uris);
+
+    foreach($latest_post_uris as $uri){
+      $result = get($ep, $uri);
+      $content = $result['content'];
+      $resource = $content->resource($uri);
+      $latest_posts[$uri] = $resource;
+    }
+    foreach($last_of_derp as $type => $uri){
+      $result = get($ep, $uri);
+      $content = $result['content'];
+      $resource = $content->resource($uri);
+      $last_of_type[$type] = $resource;
+    }
 
     ?>
 
@@ -126,22 +106,21 @@ try {
     <div id="me" class="clearfix" resource="#me" typeof="as:Person">
       <h1>
         <img src="https://rhiaro.co.uk/stash/dp.png" alt="rhiaro" rel="as:image" />
-         ... brb ...
+         ... tampering ...
       </h1>
       <div class="w1of2">
-        <?foreach($latest_posts as $post):?>
-          <p><a href="<?=$post?>"><?=$post?></a></p>
+        <?foreach($latest_posts as $uri => $resource):?>
+          <? include 'views/article.php'; ?>
         <?endforeach?>
-
+        <nav><p><a href="<?=$next?>">Next</a></p></nav>
       </div>
       <div class="w1of2">
         <p>IRL I am <span property="as:name">Amy</span></p>
         <p>On twitter I am <a href="https://twitter.com/rhiaro" rel="me">@rhiaro</a></p>
-        <p>On github I am <a href="https://github.com/rhiaro" rel="me">rhiaro</a></p>
+        <p>I store code on <a href="https://github.com/rhiaro" rel="me">github</a> and <a href="https://bitbucket.org/rhiaro">bitbucket</a></p>
         <p>By email I am <a href="mailto:amy@rhiaro.co.uk" rel="me">amy@rhiaro.co.uk</a></p>
-      <?foreach($last_of_derp as $type => $resource):?>
-        <p><a href="<?=$resource?>"><?=$resource?></a></p>
-        <?//include 'views/profile_post.php';      ?>
+      <?foreach($last_of_type as $type => $resource):?>
+        <? include 'views/profile_post.php';      ?>
       <?endforeach?>
       <h3>The 128 things I write about most are:</h3>
       <? $i = 0; ?>
