5608309afb37589afe1d42cc6fa134e06ea685df
rhiaro <amy@rhiaro.co.uk>
2016-09-11 15:14:11 -0400

    Viewport

--- views/top.php
+++ views/top.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" type="text/css" href="/views/normalize.min.css" />
     <link rel="stylesheet" type="text/css" href="/views/core.css" />
     <link rel="stylesheet" type="text/css" href="/<?=$resource->get('view:stylesheet')?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
       <?=$resource->get('view:css')?>
     </style>
