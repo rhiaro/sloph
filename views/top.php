3a6f54e01f0b6f739d7fda84150620362022514b
rhiaro <amy@rhiaro.co.uk>
2016-09-16 22:52:13 +0100

    Fix some RDFa, towards #5, thanks @csarven

--- views/top.php
+++ views/top.php
@@ -1,13 +1,13 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
   <head>
     <title><?=$resource->get('as:name')?></title>
     <link rel="stylesheet" type="text/css" href="/views/normalize.min.css" />
     <link rel="stylesheet" type="text/css" href="/views/core.css" />
     <link rel="stylesheet" type="text/css" href="/<?=$resource->get('view:stylesheet')?>" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <style>
       <?=$resource->get('view:css')?>
     </style>
   </head>
-  <body>
+  <body about="https://rhiaro.co.uk/" typeof="foaf:PersonalProfileDocument as:Profile" prefix="as: http://www.w3.org/ns/activitystreams# foaf: http://xmlns.com/foaf/0.1/ ldp: http://www.w3.org/ns/ldp# view: https://terms.rhiaro.co.uk/view# asext: https://terms.rhiaro.co.uk/as#">
