24801b1763ed5302b167cb27e1d481ed3050abfb
rhiaro <amy@rhiaro.co.uk>
2016-12-20 13:25:30 +0800

    add in reply to and uncomment tags

--- views/article.php
+++ views/article.php
@@ -2,11 +2,15 @@
   <?if(has_type($resource, 'as:Object') || has_type($resource, 'as:Event') || has_type($resource, 'as:Place') || has_type($resource, 'as:Article')):?>
     <h1><?=get_value($resource, 'as:name')?></h1>
   <?endif?>
+
+  <?if(get_value($resource, 'as:inReplyTo')):?>
+    <p><em>@ <a href="<?=get_value($resource, 'as:inReplyTo')?>">In reply to this</a>:</em></p>
+  <?endif?>
   
   <?=get_value($resource, 'as:summary')?>
   <?=get_value($resource, 'as:content')?>
   
-  <? //include('tags.php'); ?>
+  <? include('tags.php'); ?>
   <?if(get_value($resource, 'as:published')):?>
     <? $date = new DateTime(get_value($resource, 'as:published')); ?>
     <p><time><a href="<?=str_replace("https://rhiaro.co.uk", "", get_uri($resource))?>"><?=$date->format("d M Y, H:i (e)")?></a></time></p>
