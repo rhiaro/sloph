8a3c5bcfe1eb042335a31e72d95706fb8dbc0af7
rhiaro <amy@rhiaro.co.uk>
2016-12-23 16:44:52 +0800

    No error if no contents

--- views/incoming.php
+++ views/incoming.php
@@ -1,3 +1,10 @@
+<?
+// Moderation
+// /incoming/#public
+// /incoming/#private
+// can eventually use this for more granular acl, and on a triple level. things can be in more than one graph.
+// can check access token and determine which graph to fetch from
+?>
 <!doctype html>
 <html>
   <head>
@@ -16,7 +23,7 @@
   <body>
     <article>
       <h1>Notifications</h1>
-      <p>This is the API endpoint to which I receive notifications. Notifications which are hosted publicly and externally are listed here. Notifications which are not go through manual modification and may or may not be listed publicly. A full list of notifications can be retreived with the appropriate <code>Authorization</code> header.</p>
+      <p>This is the API endpoint to which I receive notifications. Notifications which are hosted publicly and externally are listed here. Notifications which aren't go through manual moderation and may or may not be listed publicly. A full list of notifications can be retreived with the appropriate <code>Authorization</code> header.</p>
       <p>Notifications are processed and published according to <a href="https://www.w3.org/TR/ldn/">Linked Data Notifications</a> for receiving.</p>
 
       <form method="post">
@@ -26,12 +33,15 @@
           <input type="submit" value="Send" />
         </p>
       </form>
-
-      <ul>
-        <?foreach($contains["https://rhiaro.co.uk/incoming/"]["http://www.w3.org/ns/ldp#contains"] as $item):?>
-          <li><a href="$item["value"]"><?=$item["value"]?></a></li>
-        <?endforeach?>
-      </ul>
+      <?if(isset($contains["https://rhiaro.co.uk/incoming/"]["http://www.w3.org/ns/ldp#contains"]) && !empty($contains["https://rhiaro.co.uk/incoming/"]["http://www.w3.org/ns/ldp#contains"])):?>
+        <ul>
+          <?foreach($contains["https://rhiaro.co.uk/incoming/"]["http://www.w3.org/ns/ldp#contains"] as $item):?>
+            <li><a href="$item["value"]"><?=$item["value"]?></a></li>
+          <?endforeach?>
+        </ul>
+      <?else:?>
+        <p>Nothing here :(</p>
+      <?endif?>
 
     </article>
   </body>
