e46e0924fa0ed7545ffe535d9285dc17e68a016d
rhiaro <amy@rhiaro.co.uk>
2017-01-02 23:29:26 +0800

    Writing, most of travel, and tempalte for the rest

--- vendor/sloph/summary.php
+++ vendor/sloph/summary.php
@@ -1,28 +1,157 @@
 <?
 require_once('../init.php');
 
-function aggregate_checkins($ep, $from, $to){
-
+function get_posts($ep, $from, $to){
+  $q = construct_between($from, $to);
+  $res = execute_query($ep, $q);
+  if($res){
+    return $res;
+  }else{
+    var_dump($res);
+    return array();
+  }
 }
 
-function aggregate_acquires($ep, $from, $to){
-
+function get_type($posts, $type){
+  $out = array();
+  foreach($posts as $uri => $post){
+    if(has_type(array($uri=>$post), $type)){
+      $out[$uri] = $post;
+    }
+  }
+  return $out;
 }
 
-function aggregate_consumes($ep, $from, $to){
+function aggregate_checkins($posts, $from, $to, $locations){
+  $typed = get_type($posts, "as:Arrive");
+  $typed = array_reverse($typed);
+  $num = array();
+  foreach($typed as $uri => $post){
+    $location = get_value(array($uri => $post), "as:location");
+    if(array_key_exists($location, $locations)){
+      // Only count vague checkins
+      $post['uri'] = $uri;
+      $num[] = $post;
+    }
+  }
+  
+  // Total durations
+  $tally = array();
+  foreach($num as $i => $post){
+    $location = get_value(array($post['uri'] => $post), "as:location");
+    $date = new DateTime(get_value(array($post['uri']=>$post), "as:published"));
+    if(isset($num[$i+1])){
+      $next = $num[$i+1];
+      $nextdate = new DateTime(get_value(array($next['uri']=>$next), "as:published"));
+    }else{
+      $nextdate = $to;
+    }
+    $seconds = $nextdate->getTimestamp() - $date->getTimestamp();
+    if(isset($tally[$location])){
+      $tally[$location] += $seconds;
+    }else{
+      $tally[$location] = $seconds;
+    }
 
+  }
+  $out['top'] = array();
+  arsort($tally);
+  foreach($tally as $location => $seconds){
+    $d1 = new DateTime("@$seconds");
+    $d2 = new DateTime("@0");
+    $duration = time_diff_to_human($d1, $d2);
+    $out['top'][] = array("location" => $location, "duration" => $duration, "label" => str_replace("her", "my", str_replace("is ", "", get_value(array($location=>$locations[$location]), "blog:presentLabel"))));
+  }
+
+  $out['total'] = count($typed);
+  return $out;
 }
 
-function aggregate_writing($ep, $from, $to){
+function aggregate_acquires($posts, $from, $to){
+  $typed = get_type($posts, "asext:Acquire");
+  $out['total'] = count($typed);
+  return $out;
+}
+
+function currency_to_gbp($date, $currency, $amount){
+  $d = $date->format("Y-m-d");
+  $ep = "https://api.fixer.io/".$d."?base=GBP";
+  $rates = file_get_contents($ep);
+  $rates = json_decode($rates, true);
+  if(isset($rates["rates"][strtoupper($currency)])){
+    $rate = $rates["rates"][strtoupper($currency)];
+    return $amount / $rate;
+  }
+  return false;
+}
+
+function aggregate_consumes($posts, $from, $to){
+  $typed = get_type($posts, "asext:Consume");
+  $out['total'] = count($typed);
+  return $out;
+}
+
+function aggregate_writing($posts, $from, $to, $alltags){
+
+  $diff = $from->diff($to);
+  $days = $diff->format('%a');
+
+  $articles = get_type($posts, "as:Article");
+  $notes = get_type($posts, "as:Note");
+  $typed = array_merge($articles, $notes);
+
+  // Counts
+  $out['total'] = count($typed);
+  $out['articles'] = count($articles);
+  $out['notes'] = count($notes);
+  
+  $out['words'] = 0;
+  foreach($typed as $uri => $post){
+    $content = get_value(array($uri=>$post), "as:content");
+    $words = explode(" ", $content);
+    $out['words'] = count($words) + $out['words'];
+  }
+
+  $out['dailywords'] = $out['words'] / $days;
+  $out['dailynotes'] = $out['total'] / $days;
+
   // Tags
-  // Specific themes: phd, socialwg, hacking, vegan, travel, star trek
+  $tags = array();
+  foreach($typed as $uri=>$post){
+    $ts = get_values(array($uri=>$post), "as:tag");
+    if(is_array($ts)){
+      foreach($ts as $t){
+        if(substr($t, 0, 4) == "http"){
+          if(isset($tags[$t])){
+            $tags[$t]++;
+          }else{
+            $tags[$t] = 1;
+          }
+        }
+      }
+    }
+  }
+  arsort($tags);
+  $top = array_slice($tags, 0, 11, true);
+  $last = array_slice($tags, 11, 1, true);
+  $out['toptags'] = "";
+  foreach($top as $t => $c){
+    $out['toptags'] .= "<a href=\"$t\">".$alltags[$t]["name"]."</a> (".$c."), ";
+  }
+  $out['toptags'] .= " and <a href=\"".key($last)."\">".$alltags[key($last)]["name"]."</a> (".$last[key($last)].")";
+  $out['tags'] = count($tags);
+
+  return $out;
 }
 
-function aggregate_socials($ep, $from, $to){
+function aggregate_socials($posts, $from, $to){
   // Likes
   // Shares
   // Bookmarks
   // Follows?
+  $typed = array_merge(get_type($posts, "as:Like"), get_type($posts, "as:Announce"), get_type($posts, "as:Add"), get_type($posts, "as:Follow"));
+  $out['total'] = count($typed);
+  return $out;
 }
 
 $now = new DateTime();
@@ -37,11 +166,16 @@ if(isset($_GET['to'])){
   $to = $now;
 }
 
-$checkins = aggregate_checkins($ep, $from, $to);
-$acquires = aggregate_acquires($ep, $from, $to);
-$consumes = aggregate_consumes($ep, $from, $to);
-$writing = aggregate_writing($ep, $from, $to);
-$socials = aggregate_socials($ep, $from, $to);
+$posts = get_posts($ep, $from->format(DATE_ATOM), $to->format(DATE_ATOM));
+$tags = get_tags($ep);
+$locations = get_locations($ep);
+$locations = $locations->toRdfPhp();
+
+$checkins = aggregate_checkins($posts, $from, $to, $locations);
+$acquires = aggregate_acquires($posts, $from, $to);
+$consumes = aggregate_consumes($posts, $from, $to);
+$writing = aggregate_writing($posts, $from, $to, $tags);
+$socials = aggregate_socials($posts, $from, $to);
 $total = $checkins['total'] + $acquires['total'] + $consumes['total'] + $writing['total'] + $socials['total'];
 
 include '../../views/summary.php';
--- views/summary.php
+++ views/summary.php
@@ -9,7 +9,33 @@
   <body>
     <article>
       <h1>from <?=$from->format("d M y")?> to <?=$to == $now ? "now" : $to->format("d M y")?></h1>
-      <p>
+      <p>I posted to my site <?=number_format($total)?> times.</p>
+      <h2>Writing</h2>
+      <p>I wrote <?=number_format($writing['total'])?> things on my site. <?=number_format($writing['notes'])?> were short notes, <?=number_format($writing['articles'])?> were longer articles, and they comprise approximately <?=number_format($writing['words'])?> words in total. That's a mean of <?=number_format($writing['dailywords'], 3)?> words and <?=number_format($writing['dailynotes'], 3)?> posts per day.</p>
+      <p>I wrote about <?=$writing['tags']?> different topics, with the most common being <?=$writing['toptags']?>.</p>
+
+      <h2>Travel</h2>
+      <p>I checked in <?=number_format($checkins['total'])?> times. I spent the most time <a href="<?=$checkins['top'][0]['location']?>"><?=$checkins['top'][0]['label']?></a>, which was <?=$checkins['top'][0]['duration']?>, followed by <?=$checkins['top'][1]['duration']?> <a href="<?=$checkins['top'][1]['location']?>"><?=$checkins['top'][1]['label']?></a>. I also spent
+      <?for($i=2;$i<count($checkins['top'])-1;$i++):?>
+        <?=$checkins['top'][$i]['duration']?> <a href="<?=$checkins['top'][$i]['location']?>"><?=$checkins['top'][$i]['label']?></a>; 
+      <?endfor?>
+      and was <a href="<?=$checkins['top'][count($checkins['top'])-1]['location']?>"><?=$checkins['top'][count($checkins['top'])-1]['label']?></a> for <?=$checkins['top'][count($checkins['top'])-1]['duration']?>.
+      </p>
+      <p>I planned x journeys, to y different places. I travelled primarily by x, followed by y and z. Some places I visited are a, b, c, d, e and f.</p>
+
+      <h2>Acquisitions</h2>
+
+      <p>I purchased or otherwise acquired something on x occasions, spending a total of &pound;y. I used z different currencies (USD, GBP, JPY, EUR, MYR, ...). This is an average expenditure of x per week, or z per month. I mostly bought [food] (%) of which % was takeaway and % was in restaurants. I spent x on groceries and y on household. I also spent z on travel or transit. On x occasions I got something for free. The most expensive thing I bought was x and the cheapest thing (which wasn't free) was y. I spent on average z per time. Other categories of expenditure are: ... .</p>
+
+      <p>% of my acquire posts have photos attached. You can see them all at <a href="/stuff">/stuff</a>.</p>
+
+      <h2>Consumption</h2>
+      <p>I logged x meals or snacks, an average of y per day. The thing I consumed most was [probably coffee] (x instances), followed by [bread] and [vegetables].</p>
+      <p>Other things I ate were: ... . You can see them all at <a href="/eats">/eats</a>.</p>
+
+      <h2>Socialling</h2>
+
+      <p>I <a href="/likes">liked</a> x links, y% of which were from Twitter. I <a href="/bookmarks">bookmarked</a> x links, and posted y images to collections over z occasions. I <a href="/reposts">reposted</a> something x times. y of my posts were in reply to something else.</p>
 
     </article>
   </body>
