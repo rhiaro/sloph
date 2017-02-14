a298224ffaa93ad8ca67c0edad6a674e8b67f66a
rhiaro <amy@rhiaro.co.uk>
2017-02-14 10:22:53 +0800

    Improve rdfa

--- views/article.php
+++ views/article.php
@@ -1,24 +1,28 @@
-<article class="h-entry">
+<article class="h-entry" typeof="as:Article" about="">
   <?if(has_type($resource, 'as:Object') || has_type($resource, 'as:Event') || has_type($resource, 'as:Place') || has_type($resource, 'as:Article')):?>
-    <h1 class="p-name"><?=get_value($resource, 'as:name')?></h1>
+    <h1 class="p-name" property="as:name"><?=get_value($resource, 'as:name')?></h1>
   <?endif?>
 
-  <?if(get_value($resource, 'as:inReplyTo')):?>
-    <p><em>@ <a class="u-in-reply-to" href="<?=get_value($resource, 'as:inReplyTo')?>">In reply to this</a>:</em></p>
+  <?if(get_values($resource, 'as:inReplyTo')):?>
+    <p><em>In reply to:
+      <?foreach(get_values($resource, 'as:inReplyTo') as $r):?>
+        <a class="u-in-reply-to" rel="as:inReplyTo" href="<?=$r?>"><?=$r?></a> 
+      <?endforeach?>
+    </em></p>
   <?endif?>
   
-  <div class="p-summary" style="display: none"><?=get_value($resource, 'as:summary')?></div>
-  <div class="e-content"><?=get_value($resource, 'as:content')?></div>
+  <div class="p-summary" property="as:summary" style="display: none"><?=get_value($resource, 'as:summary')?></div>
+  <div class="e-content" property="as:content"><?=get_value($resource, 'as:content')?></div>
   
   <? include('tags.php'); ?>
   <?if(get_value($resource, 'as:published')):?>
     <? $date = new DateTime(get_value($resource, 'as:published')); ?>
-    <p><time class="dt-published"><a class="u-url" href="<?=str_replace("https://rhiaro.co.uk", "", get_uri($resource))?>"><?=$date->format("d M Y, H:i (e)")?></a></time></p>
+    <p><time class="dt-published"><a property="as:published" class="u-url" href="<?=str_replace("https://rhiaro.co.uk", "", get_uri($resource))?>"><?=$date->format("d M Y, H:i (e)")?></a></time></p>
   <?endif?>
 
   <?if(get_value($resource, 'as:updated')):?>
     <? $date = new DateTime(get_value($resource, 'as:updated')); ?>
-    <p><em>Last modified: </em><time class="dt-updated"><a href="<?=str_replace("https://rhiaro.co.uk", "", get_uri($resource))?>"><?=$date->format("d M Y, H:i (e)")?></a></time></p>
+    <p><em>Last modified: </em><time class="dt-updated"><a property="as:updated" href="<?=str_replace("https://rhiaro.co.uk", "", get_uri($resource))?>"><?=$date->format("d M Y, H:i (e)")?></a></time></p>
   <?endif?>
   
 </article>
--- views/top.php
+++ views/top.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <title><?=get_value($resource, 'as:name')?></title>
     <link rel="stylesheet" type="text/css" href="/views/normalize.min.css" />
