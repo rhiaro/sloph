4e2bea94cbe58abf7e2b1a08b194a8706369f784
rhiaro <amy@rhiaro.co.uk>
2017-07-09 22:08:44 +0200

    Graph it up

--- scripts/files_to_graph.php
+++ scripts/files_to_graph.php
@@ -1,38 +1,48 @@
-<?
-set_time_limit(360);
+// <?
+// phpinfo();
+// die();
+// set_time_limit(3600);
 require_once('../vendor/init.php');
 
 $posts = array();
 $graph = new EasyRdf_Graph();
-$dir = "newdumpssince29july";
+// $dir = "newdumpssince29july";
+$dir = "../data/graphs_201705";
+// $dir = "../data/graphstest";
 
 $files = scandir($dir);
 foreach($files as $file){
   if($file != "." && $file != ".."){
 /* 1. Check files parse as turtle */
-    // try{
-    //   $ttl = file_get_contents("$dir/".$file);
-    //   $graph->parse($ttl, 'ttl');
-    //   // var_dump($graph->serialise('php'));
-    //   echo $file."<br/>";
-      
-    // }catch(Exception $e){
-    //   echo "Failed: ".$file."<br/>";
-    //   var_dump($e->getMessage());
-    //   echo "<br/>";
-    // }
+    try{
+        $ttl = file_get_contents("$dir/".$file);
+        // $graph->parse($ttl, 'ttl'); // Parsing is for losers anyway
+        echo $file."<br/>";
+        echo "<pre style='height: 200px; overflow: scroll'>".htmlentities($ttl)."</pre>";
+        echo "<p><a href='?file=".$file."'>Store</a></p>";
+        // echo $graph->dump();
 /* 2. Parse files and insert */
-    // $ttl = file_get_contents("$dir/".$file);
-    // $graph->parse($ttl, 'turtle');
-    // $q = query_load("$dir/".$file);
-    // echo htmlentities($q);
-    // $r = execute_query($ep, $q);
-    // if($r){
-    //   echo "Success: ".$file."<br/>";
-    // }else{
-    //   echo "Failed: ".$file;
-    // }
-    // echo "<hr/>";
+        if(isset($_GET['file']) && $_GET['file'] == $file){
+            echo "BLAH";
+            $graphname = str_replace("%", "/", $file);
+            $parts = explode("__", $graphname);
+            $graphname = $parts[0];
+            $q = query_insert_n($ttl, $graphname);
+            echo "<pre style='height: 200px; overflow: scroll'>".htmlentities($q)."</pre>";
+            $r = execute_query($ep, $q);
+            if($r){
+              echo "<p><strong>Success: ".$graphname."</strong></p>";
+            }else{
+              echo "<p><strong>Failed: ".$file."</strong></p>";
+            }
+        }
+      
+    }catch(Exception $e){
+      echo "Failed: ".$file."<br/>";
+      var_dump($e->getMessage());
+      echo "<br/>";
+    }
+    echo "<hr/>";
   }
 }
 
--- scripts/query_to_files.php
+++ scripts/query_to_files.php
@@ -3,42 +3,70 @@ require_once('../vendor/init.php');
 
 function write($data, $fn){
     
-  $log = "newdumpssince29july/$fn.ttl";
+  $log = "../data/graphs_201705/$fn";
 
-  $h = fopen($log, 'w');
+  $h = fopen($log, 'a');
   fwrite($h, $data);
-  var_dump($h);
+  // var_dump($h);
   fclose($h);
   return $log;
 }
 
-$posts = array();
-
-$q = query_select_s();
-$r = execute_query($ep, $q);
-if($r){
-  $uris = select_to_list($r, array("uri"));
-  
-  foreach($uris as $uri){
-    if(is_string($uri)){
-      $fn = mb_ereg_replace("([^\w\s\d\-_~,;\[\]\(\).])", '_', $uri);
-      if(!file_exists("newdumpssince29july/$fn.ttl")){
-        $q = query_construct($uri);
-        $t = execute_query($ep, $q);
-        if($t){ 
-          foreach($t as $uri => $stuff){
-            $https = str_replace("http://rhiaro.co.uk", "https://rhiaro.co.uk", $uri);
-            unset($t[$uri]);
-            $t[$https] = $stuff;
-          }
-          $graph = new EasyRdf_Graph();
-          $graph->parse($t, 'php');
-          $f = write($graph->serialise('text/turtle'), $fn);
-          echo "<p>$f</p>";
-        }
-      }
+function select_triple($ep){
+  $all = array();
+  $q = "SELECT ?g ?s ?p ?o
+WHERE { GRAPH ?g { ?s ?p ?o . } }
+ORDER BY ?g
+OFFSET 100000
+LIMIT 50000
+  ";
+  $r = execute_query($ep, $q);
+  $parser = ARC2::getRDFParser();
+  foreach($r["rows"] as $res){
+      $ttl = $parser->toNTriples(array($res));
+    if(!array_key_exists($res["g"], $all)){
+      $all = array_merge($all, array($res["g"] => array($ttl)));
+    }else{
+      $all[$res["g"]][] = $ttl;
     }
   }
+
+  foreach($all as $graph => $triples){
+    $data = implode("\n", $triples);
+    echo write($data, str_replace("/", "%", $graph))."\n";
+  }
+
 }
+
+select_triple($ep);
+
+// $posts = array();
+
+// $q = query_select_s();
+// $r = execute_query($ep, $q);
+// if($r){
+//   $uris = select_to_list($r, array("uri"));
+  
+//   foreach($uris as $uri){
+//     if(is_string($uri)){
+//       $fn = mb_ereg_replace("([^\w\s\d\-_~,;\[\]\(\).])", '_', $uri);
+//       if(!file_exists("newdumpssince29july/$fn.ttl")){
+//         $q = query_construct($uri);
+//         $t = execute_query($ep, $q);
+//         if($t){ 
+//           foreach($t as $uri => $stuff){
+//             $https = str_replace("http://rhiaro.co.uk", "https://rhiaro.co.uk", $uri);
+//             unset($t[$uri]);
+//             $t[$https] = $stuff;
+//           }
+//           $graph = new EasyRdf_Graph();
+//           $graph->parse($t, 'php');
+//           $f = write($graph->serialise('text/turtle'), $fn);
+//           echo "<p>$f</p>";
+//         }
+//       }
+//     }
+//   }
+// }
 
 ?>
