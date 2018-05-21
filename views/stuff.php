c0522ed6e9046965e911475ac03039594c4ed871
rhiaro <amy@rhiaro.co.uk>
2018-05-21 16:31:35 +0200

    Display alternative currencies when available

--- views/stuff.php
+++ views/stuff.php
@@ -1,5 +1,18 @@
 <?
 $date = new DateTime(get_value($resource, 'as:published'));
+$amounts = array();
+if(get_value($resource, 'asext:amountEur')){
+  $amounts[] = get_value($resource, 'asext:amountEur')." EUR";
+}
+if(get_value($resource, 'asext:amountUsd')){
+  $amounts[] = get_value($resource, 'asext:amountUsd')." USD";
+}
+if(get_value($resource, 'asext:amountGbp')){
+  $amounts[] = get_value($resource, 'asext:amountGbp')." GBP";
+}
+if(!empty($amounts)){
+  $amounts = implode(" / ", $amounts);
+}
 ?>
 <article>
   <p><datetime><a href="<?=str_replace("https://rhiaro.co.uk", "", get_uri($resource))?>"><?=$date->format("l \\t\h\\e jS \o\\f F \a\\t g:ia (e)")?></a></datetime></p>
@@ -16,6 +29,8 @@ $date = new DateTime(get_value($resource, 'as:published'));
     <?=get_value($resource, 'as:name') ? "<p><strong>".get_value($resource, 'as:name')."</strong></p>" : "" ?>
     <?=get_value($resource, 'as:content') ? "<p>".get_value($resource, 'as:content')."</p>" : "" ?>
     <?=get_value($resource, 'asext:cost') ? "<p><strong>".get_value($resource, 'asext:cost')."</strong></p>" : "" ?>
+
+    <?=$amounts ? '<p class="wee">('.$amounts.')</p>' : "" ?>
   </div>
 
   <? include('tags.php'); ?>
