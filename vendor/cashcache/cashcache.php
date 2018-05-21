e3b902ad82811756ff5f371c95fb720a90f281a9
rhiaro <amy@rhiaro.co.uk>
2018-05-21 15:44:16 +0200

    Update CashCache

--- vendor/cashcache/cashcache.php
+++ vendor/cashcache/cashcache.php
@@ -2,9 +2,12 @@
 require_once("settings.php");
 
 function get_fixer_rates($date, $currencies){
+    // Rate limit: 1000 reqs/mo
+    // Currencies: 170
+    // Base: EUR
     global $FIXERAPI;
     if(is_array($currencies)){
-        $currencies = implode($currencies, ",");
+        $currencies = implode(",", $currencies);
     }
     $date = $date->format("Y-m-d");
     $endpoint = "http://data.fixer.io/api/$date?access_key=$FIXERAPI&symbols=USD,GBP,$currencies";
@@ -14,19 +17,40 @@ function get_fixer_rates($date, $currencies){
 }
 
 function get_fixer_deprecated($date, $currencies){
-    // Deprecated API 2018-06-01 but no rate limit, good for testing til then
+    // Deprecated API 2018-06-01
+    // Rate limit: none
+    // Currencies: 32
+    // Base: any
     if(is_array($currencies)){
-        $currencies = implode($currencies, ",");
+        $currencies = implode(",", $currencies);
     }
     $date = $date->format("Y-m-d");
-    $endpoint = "https://api.fixer.io/$date?base=EUR&symbols=$currencies";
+    $endpoint = "https://api.fixer.io/$date?base=EUR&symbols=USD,GBP,$currencies";
     $rates = file_get_contents($endpoint);
     $rates = json_decode($rates, true);
     return array("EUR" => $rates["rates"]);
 }
 
 function get_currencylayer_rates($date, $currencies){
-    // TODO;
+    // Rate limit: 1000 reqs/mo
+    // Currencies: 168
+    // Base: USD
+    global $CURRENCYAPI;
+    if(!is_array($currencies)){
+        $currencies = explode(",", $currencies);
+    }
+    $currencies[] = "EUR";
+    $date = $date->format("Y-m-d");
+    $endpoint = "http://apilayer.net/api/historical?access_key=$CURRENCYAPI&date=$date";
+    $response = file_get_contents($endpoint);
+    $response = json_decode($response, true);
+    $rates = array();
+    foreach($currencies as $currency){
+      if(isset($response["quotes"]["USD".$currency])){
+        $rates[$currency] = $response["quotes"]["USD".$currency];
+      }
+    }
+    return array("USD" => $rates);
 }
 
 function read_rates($date){
@@ -58,13 +82,13 @@ function write_rates($date, $rates, $base="EUR"){
     $data = array("date" => $datef);
 
     $fn = $RATESPATH.$datef;
+    $updated = $rates[$base];
     if(file_exists($fn)){
         $existing = json_decode(file_get_contents($fn), true);
+        $data = array_merge($existing, $data);
 
         if(isset($existing[$base]) && is_array($existing[$base])){
             $updated = array_merge($existing[$base], $rates[$base]);
-        }else{
-            $updated = $rates[$base];
         }
     }
     $data[$base] = $updated;
@@ -84,14 +108,46 @@ function get_and_write($date, $currency, $base="EUR"){
 
 function convert_eur_to_any($amount, $currency, $date){
     $rate = read_rate($date, $currency);
+    if($rate == 0){
+        return null;
+    }
     $any = $amount * $rate;
     return number_format($any, 2, '.', '');
 }
 
 function convert_any_to_eur($amount, $currency, $date){
     $rate = read_rate($date, $currency);
-    $eur = $amount / $rate;
+    if($rate == 0){
+        // try converting to USD
+        $usd = convert_any_to_usd($amount, $currency, $date);
+        // convert USD to EUR
+        if($usd !== null){
+            $eur = convert_usd_to_any($usd, "EUR", $date);
+        }else{
+            return null;
+        }
+    }else{
+        $eur = $amount / $rate;
+    }
     return number_format($eur, 2, '.', '');
+}
+
+function convert_usd_to_any($amount, $currency, $date){
+    $rate = read_rate($date, $currency, "USD");
+    if($rate == 0){
+        return null;
+    }
+    $any = $amount * $rate;
+    return number_format($any, 2, '.', '');
+}
+
+function convert_any_to_usd($amount, $currency, $date){
+    $rate = read_rate($date, $currency, "USD");
+    if($rate == 0){
+        return null;
+    }
+    $usd = $amount / $rate;
+    return number_format($usd, 2, '.', '');
 }
 
 
