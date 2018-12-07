e0517b857c18c0520fbcbafe3b1c523308f9d339
rhiaro <amy@rhiaro.co.uk>
2018-12-07 23:58:14 +0200

    Improve wording

--- views/album.php
+++ views/album.php
@@ -1,7 +1,7 @@
 <article class="h-entry" typeof="as:Article" about="">
   <h1><?=get_value($resource, "as:name")?></h1>
   <?=get_value($resource, "as:content")?>
-  <p><em>Contains <?=get_value($resource, "as:totalItems")?> photos and was last updated: <?=time_ago(get_value($resource, "as:updated"))?>.</em></p>
+  <p><em>Contains <?=get_value($resource, "as:totalItems")?> photos, the last of which were added <?=time_ago(get_value($resource, "as:updated"))?>.</em></p>
   <?foreach($resource as $uri => $data):?>
     <?if(has_type(array($uri => $data), "as:Add")):?>
       <? $date = new DateTime(get_value(array($uri => $data), "as:published")); ?>
