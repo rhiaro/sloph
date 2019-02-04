6ef26540df39342c74c0537349e5b32e2699c2d9
rhiaro <amy@rhiaro.co.uk>
2019-02-04 22:31:40 +0200

    Timeline diff lines

--- views/timeline.php
+++ views/timeline.php
@@ -1,11 +1,8 @@
-<?
-$items = get_values($resource, "as:items");
-?>
 <article class="h-entry" typeof="as:Article" about="">
   <h1>Timeline</h1>
   <div class="w1of2"><div class="inner">
-    <?foreach($items as $item):?>
-      <p><?=get_value(array($item => $resource[$item]), "as:published")?></p>
+    <?foreach($timeline as $date => $post):?>
+      <div class="bar" style="height: <?=round($post['diff']/60)?>px;"></div>
     <?endforeach?>
   </div></div>
 </article>
