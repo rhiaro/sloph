1defe84736875feccc3612ebf4458b57314ce510
rhiaro <amy@rhiaro.co.uk>
2020-11-17 13:10:47 +0000

    views: Improve main header

--- views/header_stats.php
+++ views/header_stats.php
@@ -5,7 +5,7 @@
   <div class="projects">
     <h1><span>rhiaro</span></h1>
     <p><span>Timezone: <strong><?=current_timezone($ep);?></strong> (<?=$now->format("jS M H:i")?>)</span></p>
-    <p><span>Currently <strong><a href="<?=$checkin_summary["location_uri"]?>"><?=$checkin_summary["location"]?></a></strong> (for <?=$checkin_summary["for"]?>)</span></p>
+    <p><span>Currently <strong><a href="<?=$checkin_summary["location_uri"]?>"><?=$checkin_summary["location"]?></a></strong> (for <a href="/where"><?=$checkin_summary["for"]?></a>)</span></p>
     <p><span style="opacity: 0.8">You may know me from..</span></p>
     <?foreach($project_icons as $group):?>
       <div>
@@ -16,12 +16,12 @@
     <?endforeach?>
   </div>
   <div class="stats">
-    <p>Last ate <?=time_ago($consume_stats["published"])?> (<a href="<?=$consume_stats["uri"]?>"><?=$consume_stats["content"]?></a>)</p>
+    <p><a href="/eats">Last ate</a> <?=time_ago($consume_stats["published"])?> (<a href="<?=$consume_stats["uri"]?>"><?=$consume_stats["content"]?></a>)</p>
     <div class="stat-box"><div style="width: <?=$consume_stats["width"]?>;" class="<?=$consume_stats["color"]?>"></div></div>
     <p>Last exercised <?=time_ago($exercise_stats["published"])?></p>
     <div class="stat-box"><div style="width: <?=$exercise_stats["width"]?>;" class="<?=$exercise_stats["color"]?>"></div></div>
     <?if(isset($budget_stats["uri"])):?>
-      <p>Monthly budget <?=$budget_stats["perc"]?>% used (<a href="<?=$budget_stats["uri"]?>">last spent</a> <?=$budget_stats["cost"]?> on <?=$budget_stats["content"]?>)</p>
+      <p>Monthly budget <?=$budget_stats["perc"]?>% used (<a href="/stuff">last spent</a> <?=$budget_stats["cost"]?> on <a href="<?=$budget_stats["uri"]?>"><?=$budget_stats["content"]?></a>)</p>
     <?else:?>
       <p>Budget: nothing spent so far this month</p>
     <?endif?>
--- views/nav_header.php
+++ views/nav_header.php
@@ -3,6 +3,7 @@
     <li><a href="/">Posts</a></li>
     <li><a href="/photos">Photos</a></li>
     <li><a href="/archive">Archive</a></li>
+    <li><a href="cv">CV</a></li>
     <li><a href="/contact">Contact</a></li>
   </ul>
 </nav>
