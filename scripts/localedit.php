e69b29f7b858d2a96fd93a206672fd354063087e
rhiaro <amy@rhiaro.co.uk>
2020-12-30 11:48:45 +0000

    Add sameas to the world

--- scripts/localedit.php
+++ scripts/localedit.php
@@ -59,7 +59,8 @@ function plusproperty(){
       "https://www.w3.org/ns/activitystreams#" => array("name", "published", "updated", "summary", "content", "startTime", "endTime", "image", "inReplyTo", "location", "tag", "url", "to", "bto", "cc", "bcc", "duration", "actor", "object", "target", "origin", "result", "items", "relationship", "outbox", "inbox"),
       "https://terms.rhiaro.co.uk/as#" => array("cost", "wordCount", "expensedTo"),
       "https://terms.rhiaro.co.uk/view#" => array("banality", "intimacy", "tastiness", "informative", "wanderlust", "css", "color"),
-      "http://www.w3.org/2006/time#" => array("timeZone")
+      "http://www.w3.org/2006/time#" => array("timeZone"),
+      "http://www.w3.org/2002/07/owl#" => array("sameAs")
     );
   $out = '<p>';
   $out .= '  <label><select name="data[new]">';
--- vendor/init.php
+++ vendor/init.php
@@ -59,6 +59,7 @@ $_IMG = "https://i.amy.gy/resize/";
 $_PREF = array(
          'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
         ,'rdfs' => 'http://www.w3.org/2000/01/rdf-schema#'
+        ,'owl' => 'http://www.w3.org/2002/07/owl#'
         ,'foaf' =>  'http://xmlns.com/foaf/0.1/'
         ,'dc' => 'http://purl.org/dc/elements/1.1/'
         ,'dct' => 'http://purl.org/dc/terms/'
