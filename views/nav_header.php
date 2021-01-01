c010fbee6840de462457a8b64dffcef36ac8e289
rhiaro <amy@rhiaro.co.uk>
2021-01-01 23:52:53 +0000

    map: Link travel map

--- views/nav_header.php
+++ views/nav_header.php
@@ -2,6 +2,7 @@
   <ul>
     <li><a href="/">Posts</a></li>
     <li><a href="/photos">Photos</a></li>
+    <li><a href="/map">Map</a></li>
     <li><a href="/archive">Archive</a></li>
     <li><a href="cv">CV</a></li>
     <li><a href="/contact">Contact</a></li>
