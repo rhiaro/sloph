6e3710a7b799fb1964780c1382e2359e80c8eac5
rhiaro <amy@rhiaro.co.uk>
2022-06-04 21:32:20 +0100

    css: improve homepage CSS, for #20

--- views/nav_header.php
+++ views/nav_header.php
@@ -1,6 +1,7 @@
 <nav>
+  <a href="#">menu</a>
   <ul>
-    <li><a href="/">Posts</a></li>
+    <li><a href="/#latest">Posts</a></li>
     <li><a href="/photos">Photos</a></li>
     <li><a href="/map">Map</a></li>
     <li><a href="/archive">Archive</a></li>
