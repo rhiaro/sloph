71c7f69e135c8dd22fa9409ec30f8ea8a839da44
rhiaro <amy@rhiaro.co.uk>
2022-06-05 10:23:40 +0100

    templates: not used

--- views/boxes.php
+++
@@ -1,7 +0,0 @@
-<a href="<?=str_replace("https://rhiaro.co.uk/", "", $resource->getUri())?>">
-  <?if($resource->isA('as:Arrive')):?>
-    <div class="box" style="background-color: <?=$locations->get($resource->get('as:location'), 'view:color')?>"></div>
-  <?else:?>
-    <div class="box"><?=get_icon($resource)?></div>
-  <?endif?>
-</a>
