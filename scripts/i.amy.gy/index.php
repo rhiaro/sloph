71ba5c3a3f4a75ed01e3ec56847e67dfd22078fa
rhiaro <amy@rhiaro.co.uk>
2022-08-05 20:06:43 +0100

    img: catch error

--- scripts/i.amy.gy/index.php
+++ scripts/i.amy.gy/index.php
@@ -19,12 +19,14 @@ function make_collection(){
     $imgs = glob($dir."/IMG_*_*.jpg", GLOB_BRACE);
     if(!empty($imgs)){
       rsort($imgs);
-      $latest = str_ireplace(".jpg", "", str_ireplace("_hdr", "", explode("/IMG_", $imgs[0])));
+      $latest = str_ireplace("_1.jpg", "", str_ireplace(".jpg", "", str_ireplace("_hdr", "", explode("/IMG_", $imgs[0]))));
       $updated = DateTime::createFromFormat("Ymd_His", $latest[1]);
-      $coll["items"][] = array("id" => $cur.$dir."/",
-                               "totalItems" => count($imgs),
-                               "updated" => $updated->format(DATE_ATOM)
-                                );
+      if($updated){
+        $coll["items"][] = array("id" => $cur.$dir."/",
+                                 "totalItems" => count($imgs),
+                                 "updated" => $updated->format(DATE_ATOM)
+                                  );
+      }
     }
   }
   return $coll;
