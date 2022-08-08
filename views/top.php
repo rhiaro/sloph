26302a4711a32d3803600c0d152cae472748c637
rhiaro <amy@rhiaro.co.uk>
2022-08-08 06:40:30 +0100

    template: Fix title when no name

--- views/top.php
+++ views/top.php
@@ -2,13 +2,20 @@
 $theme = get_main_theme($ep);
 $styles = get_values($resource, 'view:stylesheet', $content->getUri());
 $outbox = get_values("https://rhiaro.co.uk/#me", "as:outbox");
+$title = get_value($resource, 'as:name', $content->getUri());
+if(!$title){
+  $title = get_value($resource, 'as:summary', $content->getUri());
+}
+if(!$title){
+  $title = "a post by rhiaro";
+}
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?=get_value($resource, 'as:name', $content->getUri())?></title>
+    <title><?=$title?></title>
     <link rel="stylesheet" type="text/css" href="/views/normalize.min.css" />
     <link rel="stylesheet" type="text/css" href="/views/core.css" />
 
